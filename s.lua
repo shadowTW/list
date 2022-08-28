c1419d0748aa8a61058fe6cac4a7279b36dd07c2
shad0www <64441627+shadowTW@users.noreply.github.com>
2022-08-28 19:13:36 +0200

    Create s.lua

---
+++ s.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 8303902695 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/shadowTW/dsadasdas/main/petposse.lua'))()
+    
+    elseif game.PlaceId == 6284583030 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/shadowTW/psx/main/psxfarmm.lua'))()
+end
